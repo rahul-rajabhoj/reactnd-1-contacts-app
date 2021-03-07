a8e30b1d418f9aad76b893a5dd2f6075791a54c6
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-03-07 12:18:28 +0530

    Added searchbar to demonstrate contolled component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,35 +2,74 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class ListContact extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: ''            
+    }
+
+    updateQuery = (query) => {
+        this.setState(()=> ({
+            query: query
+        }));
+    }
+
+    clearQuery = () => {
+        this.updateQuery('');
+    }
+
     render () {
+        const { query} = this.state;
+
+        const { contacts, onDeleteContact } = this.props;
+
+        const showContacts = (query.length === 0)
+            ? contacts
+            : contacts.filter((contact)=> ( contact.name.toLowerCase().includes(query.toLowerCase()) ))
+
         return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((person)=>(
-                    <li key={person.id} className='contact-list-item'>
-                        <div 
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${person.avatarURL})`
-                            }}
-                        >
-                        </div>
-                        <div className='contact-details'>
-                            <p>{person.name}</p>
-                            <p>{person.handle}</p>
-                        </div>
-                        <button onClick={() => this.props.onDeleteContact(person)} className="contact-remove">
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value= { this.state.query }
+                        onChange = { (event) => { this.updateQuery(event.target.value)}}
+                    />
+                </div>
+                {showContacts.length !== contacts.length && (
+                    <div className='showing-contacts'>
+                        <span>Now showing {showContacts.length} of {contacts.length} </span>
+                        <button onClick={this.clearQuery}>Show All</button>
+                    </div>
+                )}
+                <ol className='contact-list'>
+                    {showContacts.map((person)=>(
+                        <li key={person.id} className='contact-list-item'>
+                            <div 
+                                className='contact-avatar'
+                                style={{
+                                    backgroundImage: `url(${person.avatarURL})`
+                                }}
+                            >
+                            </div>
+                            <div className='contact-details'>
+                                <p>{person.name}</p>
+                                <p>{person.handle}</p>
+                            </div>
+                            <button onClick={() => onDeleteContact(person)} className="contact-remove">
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
         )
     }
-}
-
-ListContact.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ListContact
