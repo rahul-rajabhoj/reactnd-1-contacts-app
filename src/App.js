cf9f80cc3f6c1979d72cf1b96e2cbf404c6f703b
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-03-17 16:48:27 +0530

    Implemented AJAX call to remove contact from list

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ class App extends Component {
   removeContact = (contact) => {
     this.setState( (currentState) => ({
         contacts: currentState.contacts.filter(c => c.id !== contact.id)
-    }))
+    }));
+  
+    ContactsAPI.remove(contact);
   }
 
   render() {
